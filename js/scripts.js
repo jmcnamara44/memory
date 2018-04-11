6bdab9e81c607033299672852b7d6844639acae6
Jimmy McNamara <jmcnamara44@gmail.com>
2018-04-10 20:09:22 -0700

    code is sort of working how i want it at a basic level.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,83 @@
-back end
-var pairs = [];
-var game = [];
-var turnCounter = 0;
+var turn = 0;
+var comparisonArray = [];
+var clickOne;
+var clickTwo;
 
-
-//potential shuffle method:
-Array.prototype.shuffle = function () {
-  var i = this.length, j, temp;
-  while(--i > 0) {
-    j = Math.floor(Math.random() * (i+1));
-    temp = this[j];
-    this[j] = this[i];
-    this[i] = temp;
-  }
-}
-//potential board creating function
-function gameBoard() {
-  turnCounter = 0;
-  var output = "";
-  pairs.shuffle();
-  for(var i=0; i<pairs.length; i++) {
-    output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
-  }
-}
-
-//front end
 $(document).ready(function() {
-  // event.preventDefault();
+  $("#end-turn").click(function() {
+    var box1 = $("input:radio[name=cell1]:checked").val();
+    var box2 = $("input:radio[name=cell2]:checked").val();
+    var box3 = $("input:radio[name=cell3]:checked").val();
+    var box4 = $("input:radio[name=cell4]:checked").val();
+    var box5 = $("input:radio[name=cell5]:checked").val();
+    var box6 = $("input:radio[name=cell6]:checked").val();
+    var boxes = [box1, box2, box3, box4, box5, box6];
+    // $("input[name=cell1]").attr("checked", false); supposed to reset the clicked function in the radio buttons
+    var clickValue = boxes.forEach(function(element) {
+      comparisonArray.push(element)
+    });
+    var sortArray =comparisonArray.sort();
+    if (turn === 0) {
+      turn +=1;
 
-  $("form#input").click(event);
+      // boxes.forEach(function(element) {
+      //   comparisonArray.push(element)
+      // });
+    } else {
+        turn -= 1;
+        clickValue;
 
-  var name = $("#nameInput").val();
-  $("#nameDisplayId").text(name);
-  $("form#form").hide();
-  // gameBoard();
+        // boxes.forEach(function(element) {
+        //   comparisonArray.push(element)
+        // });
+      //   if (clickOne === clickTwo) {
+      //   alert("match. now jimmy needs to disable the buttons");
+      // } else {
+      //   alert("no match. the cards need to flip back over now!");
+      // }
+    }
+    console.log(turn);
+    // console.log(sortArray);
+    // if (turn === 1) {  //this is an earlier version of the click function to push the value of the clicked cell into an array
+    //   boxes.forEach(function(element) {
+    //     if (element) {
+    //       turn1Array.push(element)
+    //     }
+    //   });
+    //   turn +=1;
+    // // } else if (turn === 2){
+    // //   boxes.forEach(function(element) {
+    // //     turn2Array.push(element)
+    // //   });
+    // // }
+    // // alert(turn1Array);
+    // }
+  });
 });
+/* this is to try and refactor and make this more scalable, not sure if it's even possible
+var cells = ["cell1", "cell2", "cell3", "cell4", "cell5", "cell6"]
+var value = function(cellName) {
+$("input:radio[name=" + cellName + "]:checked").val();
+};
+console.log(value(cells[0]));
+*/
+
+//below is what kristi and i worked on 4/10
+// var pair = [];
+// function compareCards() {
+//   for(var i=0; i<=1; i++){
+//     pair.push("Y");
+//   }
+//   if (pair [0] == pair [1]){
+//     // alert("matched");
+//   } else {
+//     // alert("mismatched");
+//   }
+// }
+//
+// $(document).ready(function() {
+//   $(".table").click(compareCards);
+//   $("#end-turn").click(function() {
+//     console.log(pair);
+//   })
+// });
